8fff80b2fceab47282f2bf5722ad825a60091924
Miikka Van Goethem <miikkavangoethem@gmail.com>
2022-10-08 18:36:41 +0200

    adjusted git script

--- httpRequest.lua
+++ httpRequest.lua
@@ -1,2 +1,20 @@
-result = http.get("https://httpbin.org/get")
-print(result)
+args = {...}
+
+if(#args != 1){
+    print("Please provide a single argument containing the name of the script you would like to get.")
+    return
+}
+
+local scriptname = args[1]
+
+local url = "http://raw.githubusercontent.com/MiikkaVG/main/" .. scriptname
+
+scriptContents = http.get(url)
+
+local fileLocation =  "scripts/".. scriptname
+
+local h = fs.open(fileLocation, "w")
+
+h.write(scriptContents)
+
+h.close();
